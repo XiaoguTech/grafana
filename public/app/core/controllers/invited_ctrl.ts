0cc27725c1d9def5103b2a9786b9ac0e3a5361e2
zhongqin0820 <zhongqin0820@163.com>
2018-08-01 20:34:07 +0800

    locale controllers

--- public/app/core/controllers/invited_ctrl.ts
+++ public/app/core/controllers/invited_ctrl.ts
@@ -10,9 +10,9 @@ export class InvitedCtrl {
     $scope.navModel = {
       main: {
         icon: 'gicon gicon-branding',
-        text: 'Invite',
-        subTitle: 'Register your Grafana account',
-        breadcrumbs: [{ title: 'Login', url: 'login' }],
+        text: '邀请',
+        subTitle: '注册你的Grafana账号',
+        breadcrumbs: [{ title: '登录', url: 'login' }],
       },
     };
 
